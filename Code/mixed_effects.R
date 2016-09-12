8de106d5a7c44d875649a9d2a850a7edb797cd3a
jslingsby <jslingsby@gmail.com>
2016-09-12 12:05:24 +0200

    Tweaked mixed_effects.R

--- Code/mixed_effects.R
+++ Code/mixed_effects.R
@@ -3,39 +3,43 @@
 ######## the fire variable (rec_rate) towards redcting secies occurence 
 ##############################################################################
 ######## Compiled by Nyasha Magadzire 2016
-######## Last edited: 1 September 2016
+######## Last edited: 12 September 2016
 ######## Data: 
 ######## 1) Species trait and var contribution table for enhanced SDM
 ##############################################################################
+#if (Sys.getenv("USERNAME")=='nyasha') {setwd ("C:/PhD_GIT/Nyasha")}
+#if (Sys.getenv("USER")=='jasper') {setwd("/Users/jasper/GIT/Nyasha")}
+
 library(nlme)
-esdm <- read.csv("F:/Nyasha/Data/MaxEnt_Analysis/LME_perccont.csv", header=T)
+library(MuMIn)
 
-fit1 = lme(rec_rate ~ Fire_response, random = ~1|Pair, data=esdm, method="ML")
-summary(fit1)
+#esdm <- read.csv("F:/Nyasha/Data/MaxEnt_Analysis/LME_perccont.csv", header=T)
+esdm <- read.csv("Data/MaxEnt_Analysis/LME_perccont.csv", header=T)
 
+tesdm <- esdm[which(esdm$FAMILY%in%c("CELASTRACEAE","CUPRESSACEAE","PODOCARPACEAE")),]
 
-fit2 = lme(rec_rate ~ Growth_form, random = ~1|Fire_response, data=esdm, method="ML")
-summary(fit2)
+#Trim out trees and reclassify medium and tall shrubs to "Tall shrubs"
+sesdm <- esdm[-which(esdm$FAMILY%in%c("CELASTRACEAE","CUPRESSACEAE","PODOCARPACEAE")),]
+sesdm$Growth_form[which(sesdm$Growth_form=="Medium shrub")] <- "Tall shrub"
+sesdm <- droplevels(sesdm)
 
+#Inspect the data
+boxplot(rec_rate ~ Fire_response + Growth_form, data=esdm)
+boxplot(rec_rate ~ Fire_response + Growth_form, data=sesdm)
 
-fit3 = lme(rec_rate ~ FAMILY, random = ~1|Fire_response, data=esdm, method="ML")
-summary(fit3)
+#Fit model to full data (including the trees...)
+fit = lme(rec_rate ~ Fire_response * Growth_form, random = ~1|Pair, data=esdm, method="ML") #Run through all models
+dd <- dredge(fit)
+dd #All moodels are within 2.6 delta AUC so you can't really exclude any of them... best to just look at the full model
+#model.avg(dd, subset = delta < 4) #Does model averaging if there are many alternate models
+#summary(get.models(dd, 4)[[1]]) #best model
+summary(fit)
 
-library(car)
-library(lme4)
+#Fit model to full data (excluding the trees...)
+fit = lme(rec_rate ~ Fire_response * Growth_form, random = ~1|Pair, data=sesdm, method="ML") #Run through all models
+dd <- dredge(fit)
+dd #All moodels are within 1.06 delta AUC so you can't really exclude any of them... best to just look at the full model
+summary(fit)
 
-
-lmm1 <- lmer(rec_rate ~ Growth_form * Fire_response + (1 |FAMILY), data = esdm,REML = FALSE)
-summary(lmm1)
-Anova(lmm1)
-
-
-lmm2 <- lmer(rec_rate ~ Growth_form * Fire_response + (1 |Pair), data = esdm,REML = FALSE)
-summary(lmm2)
-Anova(lmm2)
-
-lmm3 <- lmer(rec_rate ~ Growth_form * Fire_response + (1 |Pair) + (1 |FAMILY), data = esdm,REML = FALSE)
-summary(lmm3)
-Anova(lmm3)
 
 
