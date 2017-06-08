b8e82d6e3b1d3c2ef34bf24c4376f4f4469c3b82
jslingsby <jslingsby@gmail.com>
2017-06-08 14:08:34 +0200

    Jasper fiddling with mixed_effects.R

--- Code/mixed_effects.R
+++ Code/mixed_effects.R
@@ -23,6 +23,11 @@ sesdm <- esdm[-which(esdm$FAMILY%in%c("CELASTRACEAE","CUPRESSACEAE","PODOCARPACE
 sesdm$Growth_form[which(sesdm$Growth_form=="Medium shrub")] <- "Tall shrub"
 sesdm <- droplevels(sesdm)
 
+shesdm <- sesdm
+shesdm$Growth_form <- as.character(shesdm$Growth_form)
+shesdm$Growth_form[which(shesdm$Growth_form %in% c("Tall shrub", "Low shrub"))] <- "Shrub"
+shesdm <- droplevels(shesdm)
+
 gesdm <- droplevels(esdm[which(esdm$Growth_form=="Graminoid"),])
 
 #Inspect the data
@@ -38,15 +43,17 @@ dd #All moodels are within 2.6 delta AUC so you can't really exclude any of them
 summary(fit)
 
 #Fit model to full data (excluding the trees...)
-fit = lme(rec_rate ~ Fire_response * Growth_form, random = ~1|Pair, data=sesdm, method="ML") #Run through all models
+fit = lme(rec_rate ~ Fire_response * Growth_form, random = ~1|Pair, data=shesdm, method="ML") #Run through all models
 dd <- dredge(fit)
 dd #All moodels are within 1.06 delta AUC so you can't really exclude any of them... best to just look at the full model
 summary(fit)
 
 #Let's look at the results if we don't use a random effect? (i.e. is phylogeny important here?)
 summary(lm(rec_rate ~ Fire_response * Growth_form, data=sesdm))
+summary(lm(rec_rate ~ Fire_response + Growth_form, data=sesdm))
+summary(lm(rec_rate ~ Growthform_trait, data=sesdm))
 
 #Graminoids only - just for fun
-fit = lme(rec_rate ~ Fire_response, random = ~1|Pair, data=gesdm, method="ML") #Run through all models
+fit = lme(rec_rate ~ Fire_response * Growth_form, random = ~1|Pair, data=shesdm, method="ML") #Run through all models
 summary(fit)
 
